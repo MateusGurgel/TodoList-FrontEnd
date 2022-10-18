028590913ce38ee06660e1fa74060c25e5f27828
Mateus Gurgel <mateusgurgel15@gmail.com>
2022-10-18 00:51:44 -0300

    Landing page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,46 +1,61 @@
 
 import type { NextPage } from 'next'
-import { Container, Button } from 'react-bootstrap'
+import { Container, Row, Placeholder, Card, Col, Button } from 'react-bootstrap'
 import styles from '../styles/Home.module.css'
 import Typewriter from 'typewriter-effect';
 
 const Home: NextPage = () => {
+
   return (
     <div>
       <div className={styles.header}>
-        TODO LIST
+
+        <a href="\" className={styles.logo} >TODO LIST</a>
+
+        <div className={styles.header_right}>
+
+          <a href="\login" className={styles.rigthSideContent} >Login</a> 
+          <a href="\register" className={styles.rigthSideContent} >register</a> 
+          
+        </div>
+
       </div>
-      <Container className={styles.center}>
-        <div className={styles.title}>
 
-        <Typewriter
-        onInit={(typewriter) => {
-          typewriter
-            .changeDelay(80)
-            .typeString('Work Better')
-            .pauseFor(1000)
-            .deleteChars(6)
-            .typeString("Faster")
-            .start();
-        }}
-      />
+      <div className={styles.banner}>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .changeDelay(80)
+                .typeString("Work Better")
+                .pauseFor(1000)
+                .deleteChars(6)
+                .typeString("Faster")
+                .pauseFor(1000)
+                .deleteChars(6)
+                .typeString("Smarter")
+                .start();
+            }}/>
         </div>
-        <div className={styles.content}>
 
-              <h1>improve business productivity</h1>
-            <p>Lorem ipsum dolor it amet, consectetur
-               adipiscing elit. Sed a orci et metus ultrices
-               commodo et vitae justo. Donec sit amet
-               commodo neque. Sed id tincidunt nisl. Lorem
-               ipsum dolor sit amet, consectetur adipiscing
-               elit. Donec augue orci, tincidunt eu pretium at,
-               consectetur quis tellus. Class aptent taciti
-               sociosqu ad litora torquent per conubia nostra,
-               per inceptos himenaeos. Duis volutpat nunc
-              suscipit mauris vehicula, a pellentesque libero </p>
-              <Button className={styles.centerButton}>Get Started!</Button>
+      <Container className={styles.content}>
 
-        </div>
+        <Row>
+          <Col>
+            <h1>Watch our prices!</h1>
+            <p>They don't exist cuz we are 100% open source 🤣</p>
+            <Button href='https://github.com/MateusGurgel'>Show me the source</Button>
+          </Col>
+          <Col>
+            <h1>What is a to-do list?</h1>
+            <p>It’s a list of tasks you need to complete or things that you want to do. </p>
+          </Col>
+          <Col>
+            <h1>Easy to use!</h1>
+            <p>You literally have to press just 2 buttons</p>
+            <Button>Let's get started</Button>
+          </Col>
+        </Row>
+
       </Container>
     </div>
   )
