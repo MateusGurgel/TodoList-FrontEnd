c2daf694a3fbc9f5e698984cab6a8eda094d15c3
Mateus Gurgel <mateusgurgel15@gmail.com>
2022-10-19 23:32:14 -0300

    register modal working

--- components/registerModal.tsx
+++ components/registerModal.tsx
@@ -1,6 +1,33 @@
-import { Button, Modal } from 'react-bootstrap'
+import axios from 'axios';
+import { FormEvent, useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap'
 
 function RegisterModal (props : any){
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = async (event : FormEvent) => {
+
+    //placeHolder
+
+    event.preventDefault();
+
+    const formData = new FormData(event.target as HTMLFormElement)
+    const data = Object.fromEntries(formData)
+
+    try{
+      const post = await axios.post("http://127.0.0.1:3333/users/", {
+        "username": data.username,
+        "email": data.email,
+        "password": data.password,
+      })
+      alert('Account Created with Success!')
+    }catch (err) {
+      console.error(err)
+      alert("Connection error")
+    }
+  };
+
     return (
       <Modal
         {...props}
@@ -14,11 +41,40 @@ function RegisterModal (props : any){
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Register now</h4>
+          <Form validated={validated} onSubmit={handleSubmit}>
+
+            <Form.Group className="mb-3" controlId="email">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                required
+                name='email'
+                type="email" 
+                placeholder="email@example.com" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control 
+              required
+              name='username'
+              type="text"
+              placeholder="Username" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                name="password"
+                pattern="().{8,}"
+                placeholder="Password (Minimum 8 characters)" />
+            </Form.Group>
+
+              <Button type="submit">Submit form</Button>
+
+          </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button>Send</Button>
-        </Modal.Footer>
       </Modal>
     );
   }
