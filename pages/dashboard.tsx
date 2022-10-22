26e0f0463f48b99a2b830ceab89b4e83794a48a7
Mateus Gurgel <mateusgurgel15@gmail.com>
2022-10-22 12:35:23 -0300

    DashBoard Added

---
+++ pages/dashboard.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from "next";
+import { Container, Button, Stack, Row, Col, ListGroup } from "react-bootstrap";
+import TaskItem from "../components/taskItem";
+import styles from "../styles/Dashboard.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <div className={styles.header}>
+        <a href="\" className={styles.logo}>
+          TODO LIST
+        </a>
+        <div className={styles.header_right}></div>
+      </div>
+
+      <Container className={styles.content}>
+        <Stack gap={5}>
+          <Button variant="outline-primary">Add</Button>
+          <ListGroup>
+              tasks
+          </ListGroup>
+        </Stack>
+      </Container>
+    </div>
+  );
+};
+
+export default Home;
